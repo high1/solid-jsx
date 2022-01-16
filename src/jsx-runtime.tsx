8085932b45dc3260793b4eb84c6e941bbcec13fc
high1 <high1@mailfence.com>
2022-01-16 22:20:52 +0100

    Minor refactoring

--- src/jsx-runtime.tsx
+++ src/jsx-runtime.tsx
@@ -9,10 +9,7 @@ export function jsx(
 ): Component {
   const newProperties: Record<string, unknown> = {};
 
-  for (const key of Object.keys(properties)) {
-    const newKey = jsxKeyToSolid(key);
-    newKey ? (newProperties[newKey] = properties[key]) : (newProperties[key] = properties[key]);
-  }
+  for (const key of Object.keys(properties)) newProperties[jsxKeyToSolid(key)] = properties[key];
 
   return typeof type === 'function'
     ? type.name === 'Fragment'
@@ -39,7 +36,7 @@ const MAPPED_ATTRIBUTES = new Map([
   ['noValidate', 'novalidate'],
 ]);
 
-function jsxKeyToSolid(key: string): string | undefined {
+function jsxKeyToSolid(key: string): string {
   return (
     MAPPED_ATTRIBUTES.get(key) ||
     key.replace(/^(xmlns|xlink)(.+)/, (_, p1: string, p2: string) => `${p1}:${p2.toLowerCase()}`)
