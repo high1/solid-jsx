ca3d37deedd490b7c3cc1c7ed33b9bb8f64cb3c3
high1 <high1@mailfence.com>
2022-06-12 22:37:11 +0200

    refactor: remove hardcoded property names
    - removed hardcoded property names and used regular expression from
    vobyjs
    - normalize only svg properties

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,25 +1,13 @@
-import {
-  createComponent,
-  createContext,
-  JSX,
-  mergeProps,
-  PropsWithChildren,
-  useContext,
-} from 'solid-js';
+import { createComponent, createContext, JSX, mergeProps, ParentProps, useContext } from 'solid-js';
 import { Dynamic } from 'solid-js/web';
-import { LOWERCASE_ATTRIBUTES, REPLACED_COMPAT, ATTRIBUTES_PROPERTIES_MAP } from 'elements';
-import { isFirstLetterCapital } from 'utilities';
-
-const compatRegExp = new RegExp([...REPLACED_COMPAT.keys()].join('|'), 'g');
-
-const svgXmlnsRegExp = /^(xmlns)([A-Z][a-z]+)/;
+import { isFirstLetterCapital, isSVGElement, normalizeKeySvg } from 'utilities';
 
 export const MDXContext = createContext({});
 
 export const MDXProvider = (
-  properties: PropsWithChildren<{
+  properties: ParentProps<{
     components: {
-      [k: string]: (properties_: PropsWithChildren) => JSX.Element;
+      [k: string]: (properties_: ParentProps) => JSX.Element;
     };
   }>
 ): JSX.Element =>
@@ -31,33 +19,39 @@ export const MDXProvider = (
     children: () => properties.children,
   });
 
-export const useMDXComponents = (): Record<
-  string,
-  (properties: PropsWithChildren) => JSX.Element
-> => useContext(MDXContext);
+export const useMDXComponents = (): Record<string, (properties: ParentProps) => JSX.Element> =>
+  useContext(MDXContext);
+
+const REPLACED_COMPAT_SET = new Set(['mjx']);
+const compatRegExp = new RegExp(`(?:${[...REPLACED_COMPAT_SET].join('|')})-.+`, 'g');
+
+const expressionCache: Record<string, string> = {};
+const replaceDashWithUnderscore = <T>(expression: T): string | T =>
+  typeof expression === 'string'
+    ? expressionCache[expression] ??
+      (expressionCache[expression] = expression.replace(compatRegExp, (match: string) =>
+        match.replace(/-/g, '_')
+      ))
+    : expression;
 
 const getProperties = (
-  properties: Record<string, unknown> & { children?: JSX.Element }
-): PropsWithChildren => {
+  properties: Record<string, unknown> & { children?: JSX.Element },
+  type?: string
+): ParentProps => {
   const properties_: Record<string, unknown> = {};
   for (const key of Object.keys(properties))
-    properties_[jsxKeyToSolid(key)] =
+    properties_[jsxKeyToSolid(key, type)] =
       typeof properties[key] === 'object' && !Array.isArray(properties[key])
-        ? getProperties(properties[key] as Record<string, unknown>)
-        : typeof properties[key] === 'string'
-        ? (properties[key] as string).replace(
-            compatRegExp,
-            (match: string) => REPLACED_COMPAT.get(match) ?? match
-          )
-        : properties[key];
+        ? getProperties(properties[key] as Record<string, unknown>, type)
+        : replaceDashWithUnderscore(properties[key]);
   return properties_;
 };
 
-export const Fragment = (properties: PropsWithChildren): JSX.Element => properties.children;
+export const Fragment = (properties: ParentProps): JSX.Element => properties.children;
 
 export const jsx = (
-  type: string | ((properties_: PropsWithChildren) => JSX.Element),
-  properties: PropsWithChildren
+  type: string | ((properties_: ParentProps) => JSX.Element),
+  properties: ParentProps
 ): JSX.Element =>
   typeof type === 'function'
     ? type.name === 'Fragment'
@@ -65,10 +59,15 @@ export const jsx = (
       : type(getProperties(properties))
     : createComponent(
         Dynamic,
-        mergeProps(isFirstLetterCapital(type) ? properties : getProperties(properties), {
-          component: REPLACED_COMPAT.get(type) ?? type,
+        mergeProps(isFirstLetterCapital(type) ? properties : getProperties(properties, type), {
+          component: replaceDashWithUnderscore(type),
         })
       );
+
+const jsxKeyToSolid = (key: string, type = ''): string =>
+  isSVGElement(type)
+    ? (key = key === 'xlinkHref' || key === 'xlink:href' ? 'href' : normalizeKeySvg(key))
+    : key;
 
 // For the moment we do not distinguish static children from dynamic ones
 export const jsxs = jsx;
@@ -76,8 +75,3 @@ export const jsxs = jsx;
 // For the moment there is not special development handling
 // function jsxDEV(type, props , maybeKey, isStaticChildren, source, self)
 export const jsxDEV = jsx;
-
-const jsxKeyToSolid = (key: string): string =>
-  ATTRIBUTES_PROPERTIES_MAP.get(key) ??
-  LOWERCASE_ATTRIBUTES.get(key) ??
-  key.replace(svgXmlnsRegExp, (_, p1: string, p2: string) => `${p1}:${p2.toLowerCase()}`);
--- src/utilities.ts
+++ src/utilities.ts
@@ -7,3 +7,23 @@ export const capitalizeFirstLetter = (
   [first, ...rest]: string,
   locale = !isServer && navigator.language
 ): string => [first.toLocaleUpperCase(locale), ...rest].join('');
+
+const attributeCamelCasedRegExp =
+  /e(r[HRWrv]|[Vawy])|Con|l(e[Tcs]|c)|s(eP|y)|a(t[rt]|u|v)|Of|Ex|f[XYa]|gt|hR|d[Pg]|t[TXYd]|[UZq]/; //URL: https://regex101.com/r/I8Wm4S/1
+const attributesCache: Record<string, string> = {};
+const uppercaseRe = /[A-Z]/g;
+
+export const normalizeKeySvg = (key: string): string =>
+  attributesCache[key] ||
+  (attributesCache[key] = attributeCamelCasedRegExp.test(key)
+    ? key
+    : key.replace(uppercaseRe, (char) => `-${char.toLowerCase()}`));
+
+const svgRe =
+  /^(t(ext$|s)|s[vwy]|g)|^set|tad|ker|p(at|s)|s(to|c$|ca|k)|r(ec|cl)|ew|us|f($|e|s)|cu|n[ei]|l[ty]|[GOP]/; //URL: https://regex101.com/r/Ck4kFp/1
+const svgCache: Record<string, boolean> = {};
+
+export const isSVGElement = (element: string): boolean =>
+  element in svgCache
+    ? svgCache[element]
+    : (svgCache[element] = svgRe.test(element) && !element.includes('-'));
